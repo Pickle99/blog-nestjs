6bed3e9860da5b31164f33f9a7f2f7819dfb7ddc
pickle99 <jackrestler@gmail.com>
2025-03-26 11:00:51 +0400

    feat: give columns to user table

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,15 @@
-export class User {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+@Entity({ name: 'users' }) // setting up different name for table name
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true, type: 'varchar', length: 24 })
+  username: string;
+
+  @Column({ type: 'varchar', length: 24 })
+  fullName: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  password: string;
+}
