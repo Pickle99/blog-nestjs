e51c168ebb1ef2e76263298b039a7a9a458ed02c
pickle99 <jackrestler@gmail.com>
2025-03-26 14:03:40 +0400

    feat: define relationships between user and blogs (one to many)

--- src/blogs/entities/blog.entity.ts
+++ src/blogs/entities/blog.entity.ts
@@ -1,1 +1,36 @@
-export class Blog {}
+import { User } from 'src/users/entities/user.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+@Entity({ name: 'blogs' }) // setting up different name for table name
+export class Blog {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'number' })
+  user_id: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  description: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at: Date;
+
+  @ManyToOne(() => User, (user) => user.blogs) // Define the relationship: many blogs to one user
+  @JoinColumn({ name: 'user_id' }) // Specify the column name that will store the user's ID in the blog table
+  user: User;
+
+  constructor(blog: Partial<Blog>) {
+    Object.assign(this, blog);
+  }
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,6 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
-@Entity({ name: 'users' }) // setting up different name for table name
+import { Blog } from 'src/blogs/entities/blog.entity';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+@Entity({ name: 'users' }) // Setting up different name for table name
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
@@ -8,10 +9,13 @@ export class User {
   username: string;
 
   @Column({ type: 'varchar', length: 24 })
-  fullName: string;
+  fullname: string;
 
   @Column({ type: 'varchar', length: 255 })
   password: string;
+
+  @OneToMany(() => Blog, (blog) => blog.user) // Define the relationship: one user to many blogs
+  blogs: Blog[];
 
   constructor(user: Partial<User>) {
     Object.assign(this, user);
