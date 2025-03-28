57f8161b6f6afa65b3706a4fb86070d2caef528e
pickle99 <jackrestler@gmail.com>
2025-03-28 11:44:27 +0400

    feat: create ApiOperation inside AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { LoginDto, SignupDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
-
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+@ApiTags('Blogs')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Register/Create user' })
   @Post('signup')
   async signup(@Body() signupRequest: SignupDto) {
     return this.authService.signup(signupRequest);
   }
 
+  @ApiOperation({ summary: 'Auth/Login user' })
   @Post('login')
   async login(@Body() loginRequest: LoginDto) {
     return this.authService.login(loginRequest);
